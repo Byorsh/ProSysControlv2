0c6a5d99a55d677b7be4d3c553e55637bfa71738
Byorsh <106332217+Byorsh@users.noreply.github.com>
2022-10-22 02:31:36 -0700

    Ahora si se agrego la proteccion de contrasenia y generacion de codigos aleatorios

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -9,13 +9,54 @@
     }
 
     class mainModel{
+        
         /*--------- Funcion para conectar a la BD ---------*/
-
         protected static function conectar(){
             $conexion = new PDO(SGBD, USER, PASS);
             $conexion -> exec("SET CHARACTER SET utf8");
 
             return $conexion;
-            
+
         }
+
+        /*--------- Funcion para ejecutar consultas simples ---------*/
+        protected static function ejecutar_consulta_simple($consulta){
+            $sql=self::conectar()->prepare($consulta);
+            $sql->execute();
+
+            return $sql;
+
+        }
+
+        /*--------- Funcion para encriptar cadenas ---------*/
+        public function encryption($string){
+			$output=FALSE;
+			$key=hash('sha256', SECRET_KEY);
+			$iv=substr(hash('sha256', SECRET_IV), 0, 16);
+			$output=openssl_encrypt($string, METHOD, $key, 0, $iv);
+			$output=base64_encode($output);
+			return $output;
+
+		}
+
+        /*--------- Funcion para desencriptar cadenas ---------*/
+		protected static function decryption($string){
+			$key=hash('sha256', SECRET_KEY);
+			$iv=substr(hash('sha256', SECRET_IV), 0, 16);
+			$output=openssl_decrypt(base64_decode($string), METHOD, $key, 0, $iv);
+			return $output;
+
+		}
+
+        /*--------- Funcion para generar codigos aleatorios ---------*/
+        protected static function generar_codigo_aleatorio($letra, $longitud, $numero){
+            for($i=1; $i <= $longitud; $i++){
+                $aleatorio= rand(0,9);
+                $letra.=$aleatorio;
+
+            }
+            return $letra."-".$numero;
+
+        }
+
     }
